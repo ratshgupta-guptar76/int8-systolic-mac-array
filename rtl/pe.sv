3056c53e7763716d886ff3f6e6f4b8d34fe6c82b
Ratish Gupta <guptar76@mcmaster.ca>
2026-04-14 01:17:18 -0400

    Model File seems correct. Testbenches might be the problem.

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -4,32 +4,27 @@ module pe(
 
     input logic signed [7:0] a_in,
     input logic signed [7:0] b_in,
-    input logic signed [31:0] c_in,
     input logic valid_in,
 
     output logic signed [7:0] a_out,
     output logic signed [7:0] b_out,
-    output logic signed [31:0] c_out,
-    output logic valid_out
+    output logic signed [31:0] acc,
 );
     
     always_ff @(posedge clk or negedge rst_n) begin
         if (~rst_n) begin
-            a_out <= 8'sd0;
-            b_out <= 8'sd0;
-            c_out <= 32'sd0;
-            valid_out <= 1'b0;
+            a_out <=  8'sd0;
+            b_out <=  8'sd0;
+            acc   <= 32'sd0;
         end else begin
             if (valid_in) begin
                 a_out <= a_in;
                 b_out <= b_in;
-                c_out <= (a_in * b_in) + c_in;
-                valid_out <= 1'b1;
+                acc <= (a_in * b_in) + acc;
             end else begin
                 a_out <= a_out;
                 b_out <= b_out;
-                c_out <= c_out;
-                valid_out <= 1'b0;
+                acc <= acc;
             end
         end
     end
